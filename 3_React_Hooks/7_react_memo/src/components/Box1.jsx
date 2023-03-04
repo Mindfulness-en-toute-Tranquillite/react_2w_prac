19c3debdc04b3b827829a7642e9cda86c6217c2d
Mindfulness-en-toute-Tranquillite <tvghoy@gmail.com>
2023-03-05 02:17:40 +0900

    7강 React Hooks react_memo 강의 연습.

---
+++ 3_React_Hooks/7_react_memo/src/components/Box1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const style = {
+        width: '100px',
+        height: '100px',
+        backgroundColor: '#01c49f',
+        color: 'white', 
+};
+
+function Box1() {
+    console.log('Box1 컴포넌트가 랜더링되었습니다.');
+    return <div style={style}
+    >Box1</div>;
+}
+
+export default React.memo(Box1);
